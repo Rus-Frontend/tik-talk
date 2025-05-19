240cafb12070331d23d199d8d0e95c387cebbde5
ruslan <ismrusl@mail.ru>
2025-05-19 19:25:57 +0300

    6+

--- src/app/pages/profile-page/post/post.component.ts
+++ src/app/pages/profile-page/post/post.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, inject, input, OnInit, signal} from '@angular/core';
+import {AvatarCircleComponent} from "../../../common-ui/avatar-circle/avatar-circle.component";
+import {DatePipe} from "@angular/common";
+import {SvgIconComponent} from "../../../common-ui/svg-icon/svg-icon.component";
+import {PostInputComponent} from "../post-input/post-input.component";
+import {CommentComponent} from "./comment/comment.component";
+import {PostService} from "../../../data/services/post.service";
+import {firstValueFrom} from "rxjs";
+import {Post, PostComment} from "../../../data/interfaces/post.interface";
 
 @Component({
   selector: 'app-post',
-  imports: [],
+  imports: [
+    AvatarCircleComponent,
+    DatePipe,
+    SvgIconComponent,
+    PostInputComponent,
+    CommentComponent
+  ],
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss'
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+  post = input<Post>()
 
+  comments = signal<PostComment[]>([])
+
+  postService = inject(PostService)
+
+  async ngOnInit(): Promise<void> {
+    this.comments.set(this.post()!.comments)
+  }
+
+  async onCreated() {
+    const comments = await firstValueFrom(this.postService.getCommentsByPostId(this.post()!.id))
+
+    this.comments.set(comments)
+  }
 }
